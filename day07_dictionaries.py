e8437c84717a16eb38674b3013bd769e6af10ea5
ChitraSenthilvel <115971042+ChitraSenthilvel@users.noreply.github.com>
2026-01-27 10:15:51 +0400

    Create day07_dictionaries.py

---
+++ day07_dictionaries.py
@@ -0,0 +1,16 @@
+# Day 07 - Python Restart 2026
+# Topic: Dictionaries
+
+student = {
+    "name": "Chitra",
+    "age": 22,
+    "course": "Python"
+}
+
+print(student)
+print(student["name"])
+
+student["age"] = 23
+student["country"] = "India"
+
+print(student)

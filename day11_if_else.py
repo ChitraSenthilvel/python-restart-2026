2c9bcbe9360642bd8f5574a6f55a5df20d1ac998
ChitraSenthilvel <115971042+ChitraSenthilvel@users.noreply.github.com>
2026-02-02 09:05:50 +0400

    Create day11_if_else.py

---
+++ day11_if_else.py
@@ -0,0 +1,23 @@
+# Day 11 - Python Restart 2026
+# Topic: If / Else conditions
+
+age = 18
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+# Using elif
+marks = 72
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+else:
+    grade = "D"
+
+print("Grade:", grade)
